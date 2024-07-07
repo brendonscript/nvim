92dda4e9f7f81e33a0114a9f8a56d42807db86cb
Brendon McGay <brendon.mcgay@gmail.com>
2024-07-07 19:38:30 -0400

    remove supermaven + updates

--- lua/plugins/supermaven.lua
+++ lua/plugins/supermaven.lua
@@ -1,17 +1,18 @@
 return {
-  'hrsh7th/nvim-cmp',
-  dependencies = {
-    'supermaven-inc/supermaven-nvim',
-    opts = {
-      disable_keymaps = true,
-      disable_inline_completion = true,
-      log_level = 'off',
-    },
-  },
-  opts = function(_, opts)
-    table.insert(opts.sources, 3, {
-      name = 'supermaven',
-      group_index = 1,
-    })
-  end,
+  -- 'hrsh7th/nvim-cmp',
+  -- enabled = false,
+  -- dependencies = {
+  --   'supermaven-inc/supermaven-nvim',
+  --   opts = {
+  --     disable_keymaps = true,
+  --     disable_inline_completion = true,
+  --     log_level = 'off',
+  --   },
+  -- },
+  -- opts = function(_, opts)
+  --   table.insert(opts.sources, 3, {
+  --     name = 'supermaven',
+  --     group_index = 1,
+  --   })
+  -- end,
 }
