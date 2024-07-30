2e80c0f0d1379f38e8fd3107f07803a69f6d69c9
Brendon McGay <brendon.mcgay@gmail.com>
2024-07-30 16:11:41 -0400

    yazi

---
+++ lua/plugins/yazi.lua
@@ -0,0 +1,43 @@
+return {
+  'mikavilpas/yazi.nvim',
+  lazy = true,
+  keys = {
+    {
+      '<leader>Y',
+      function()
+        require('yazi').yazi()
+      end,
+      desc = 'Open the file manager',
+    },
+    {
+      -- Open in the current working directory
+      '<leader><C-y>',
+      function()
+        require('yazi').yazi(nil, vim.fn.getcwd())
+      end,
+      desc = "Open the file manager in nvim's working directory",
+    },
+    {
+      '<c-up>',
+      function()
+        -- NOTE: requires a version of yazi that includes
+        -- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+        require('yazi').toggle()
+      end,
+      desc = 'Resume the last yazi session',
+    },
+  },
+  ---@type YaziConfig
+  opts = {
+    -- if you want to open yazi instead of netrw, see below for more info
+    open_for_directories = false,
+
+    -- enable these if you are using the latest version of yazi
+    -- use_ya_for_events_reading = true,
+    -- use_yazi_client_id_flag = true,
+
+    keymaps = {
+      show_help = '<f1>',
+    },
+  },
+}
